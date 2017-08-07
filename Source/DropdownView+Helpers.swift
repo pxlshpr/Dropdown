b84b2aa05c5eda86e502da77e1b7880f1ce7d9d8
Ahmed Khalaf <pxlshpr@gmail.com>
2017-08-07 11:36:13 +0800

    remove a redundant class

---
+++ Source/DropdownView+Helpers.swift
@@ -0,0 +1,138 @@
+import UIKit
+
+extension DropdownView {
+  
+  open func show() {
+    if self.isShown == false {
+      self.showMenu()
+    }
+  }
+  
+  open func hide() {
+    if self.isShown == true {
+      self.hideMenu()
+    }
+  }
+  
+  open func toggle() {
+    if(self.isShown == true) {
+      self.hideMenu();
+    } else {
+      self.showMenu();
+    }
+  }
+  
+  open func updateItems(_ items: [String]) {
+    if !items.isEmpty {
+      self.tableView.items = items
+      self.tableView.reloadData()
+    }
+  }
+  
+  open func setSelected(index: Int) {
+    self.tableView.selectedIndexPath = index
+    self.tableView.reloadData()
+    
+    if self.shouldChangeTitleText! {
+      self.menuTitle.text = "\(self.tableView.items[index])"
+    }
+  }
+  
+  func setupDefaultConfiguration() {
+    self.menuTitleColor = self.navigationController?.navigationBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
+    self.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
+    self.cellSeparatorColor = self.navigationController?.navigationBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
+    self.cellTextLabelColor = self.navigationController?.navigationBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
+    
+    self.arrowTintColor = self.configuration.arrowTintColor
+  }
+  
+  func showMenu() {
+    self.menuWrapper.frame.origin.y = self.navigationController!.navigationBar.frame.maxY
+    
+    self.isShown = true
+    
+    // Table view header
+    let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 300))
+    headerView.backgroundColor = self.configuration.cellBackgroundColor
+    self.tableView.tableHeaderView = headerView
+    
+    self.topSeparator.backgroundColor = self.configuration.cellSeparatorColor
+    
+    // Rotate arrow
+    self.rotateArrow()
+    
+    // Visible menu view
+    self.menuWrapper.isHidden = false
+    
+    // Change background alpha
+    self.backgroundView.alpha = 0
+    
+    // Animation
+    self.tableView.frame.origin.y = -CGFloat(self.items.count) * self.configuration.cellHeight - 300
+    
+    // Reload data to dismiss highlight color of selected cell
+    self.tableView.reloadData()
+    
+    self.menuWrapper.superview?.bringSubview(toFront: self.menuWrapper)
+    
+    UIView.animate(
+      withDuration: self.configuration.animationDuration * 1.5,
+      delay: 0,
+      usingSpringWithDamping: 0.7,
+      initialSpringVelocity: 0.5,
+      options: [],
+      animations: {
+        self.tableView.frame.origin.y = CGFloat(-300)
+        self.backgroundView.alpha = self.configuration.maskBackgroundOpacity },
+      completion: nil
+    )
+  }
+  
+  func hideMenu() {
+    // Rotate arrow
+    self.rotateArrow()
+    
+    self.isShown = false
+    
+    // Change background alpha
+    self.backgroundView.alpha = self.configuration.maskBackgroundOpacity
+    
+    UIView.animate(
+      withDuration: self.configuration.animationDuration * 1.5,
+      delay: 0,
+      usingSpringWithDamping: 0.7,
+      initialSpringVelocity: 0.5,
+      options: [],
+      animations: {
+        self.tableView.frame.origin.y = CGFloat(-200)
+    }, completion: nil
+    )
+    
+    // Animation
+    UIView.animate(
+      withDuration: self.configuration.animationDuration,
+      delay: 0,
+      options: UIViewAnimationOptions(),
+      animations: {
+        self.tableView.frame.origin.y = -CGFloat(self.items.count) * self.configuration.cellHeight - 300
+        self.backgroundView.alpha = 0 },
+      completion: { _ in
+        if self.isShown == false && self.tableView.frame.origin.y == -CGFloat(self.items.count) * self.configuration.cellHeight - 300 {
+          self.menuWrapper.isHidden = true
+        }
+    })
+  }
+  
+  func rotateArrow() {
+    UIView.animate(withDuration: self.configuration.animationDuration, animations: {[weak self] () -> () in
+      if let selfie = self {
+        selfie.menuArrow.transform = selfie.menuArrow.transform.rotated(by: 180 * CGFloat(Double.pi/180))
+      }
+    })
+  }
+  
+  func menuButtonTapped(_ sender: UIButton) {
+    self.isShown == true ? hideMenu() : showMenu()
+  }
+}
--- Source/DropdownView.swift
+++ Source/DropdownView.swift
@@ -125,16 +125,16 @@ open class DropdownView: UIView {
   open var didSelectItemAtIndexHandler: ((_ indexPath: Int) -> ())?
   open var isShown: Bool!
   
-  fileprivate weak var navigationController: UINavigationController?
-  fileprivate var configuration = DropdownUIConfiguration()
-  fileprivate var topSeparator: UIView!
-  fileprivate var menuButton: UIButton!
-  fileprivate var menuTitle: UILabel!
-  fileprivate var menuArrow: UIImageView!
-  fileprivate var backgroundView: UIView!
-  fileprivate var tableView: DropdownTableView!
-  fileprivate var items: [String]!
-  fileprivate var menuWrapper: UIView!
+  internal weak var navigationController: UINavigationController?
+  internal var configuration = DropdownUIConfiguration()
+  internal var topSeparator: UIView!
+  internal var menuButton: UIButton!
+  internal var menuTitle: UILabel!
+  internal var menuArrow: UIImageView!
+  internal var backgroundView: UIView!
+  internal var tableView: DropdownTableView!
+  internal var items: [String]!
+  internal var menuWrapper: UIView!
   
   required public init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
@@ -248,7 +248,7 @@ open class DropdownView: UIView {
     self.tableView.selectRowAtIndexPathHandler = { [unowned self] (indexPath: Int) -> () in
       self.didSelectItemAtIndexHandler!(indexPath)
       if self.shouldChangeTitleText! {
-        self.setMenuTitle("\(self.tableView.items[indexPath])")
+        self.menuTitle.text = "\(self.tableView.items[indexPath])"
       }
       self.hideMenu()
       self.layoutSubviews()
@@ -283,143 +283,5 @@ open class DropdownView: UIView {
     self.menuArrow.center = CGPoint(x: self.menuTitle.frame.maxX + self.configuration.arrowPadding, y: self.frame.size.height/2)
     self.menuWrapper.frame.origin.y = self.navigationController!.navigationBar.frame.maxY
     self.tableView.reloadData()
-  }
-  
-  open func show() {
-    if self.isShown == false {
-      self.showMenu()
-    }
-  }
-  
-  open func hide() {
-    if self.isShown == true {
-      self.hideMenu()
-    }
-  }
-  
-  open func toggle() {
-    if(self.isShown == true) {
-      self.hideMenu();
-    } else {
-      self.showMenu();
-    }
-  }
-  
-  open func updateItems(_ items: [String]) {
-    if !items.isEmpty {
-      self.tableView.items = items
-      self.tableView.reloadData()
-    }
-  }
-  
-  open func setSelected(index: Int) {
-    self.tableView.selectedIndexPath = index
-    self.tableView.reloadData()
-    
-    if self.shouldChangeTitleText! {
-      self.setMenuTitle("\(self.tableView.items[index])")
-    }
-  }
-  
-  func setupDefaultConfiguration() {
-    self.menuTitleColor = self.navigationController?.navigationBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
-    self.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
-    self.cellSeparatorColor = self.navigationController?.navigationBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
-    self.cellTextLabelColor = self.navigationController?.navigationBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
-    
-    self.arrowTintColor = self.configuration.arrowTintColor
-  }
-  
-  func showMenu() {
-    self.menuWrapper.frame.origin.y = self.navigationController!.navigationBar.frame.maxY
-    
-    self.isShown = true
-    
-    // Table view header
-    let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 300))
-    headerView.backgroundColor = self.configuration.cellBackgroundColor
-    self.tableView.tableHeaderView = headerView
-    
-    self.topSeparator.backgroundColor = self.configuration.cellSeparatorColor
-    
-    // Rotate arrow
-    self.rotateArrow()
-    
-    // Visible menu view
-    self.menuWrapper.isHidden = false
-    
-    // Change background alpha
-    self.backgroundView.alpha = 0
-    
-    // Animation
-    self.tableView.frame.origin.y = -CGFloat(self.items.count) * self.configuration.cellHeight - 300
-    
-    // Reload data to dismiss highlight color of selected cell
-    self.tableView.reloadData()
-    
-    self.menuWrapper.superview?.bringSubview(toFront: self.menuWrapper)
-    
-    UIView.animate(
-      withDuration: self.configuration.animationDuration * 1.5,
-      delay: 0,
-      usingSpringWithDamping: 0.7,
-      initialSpringVelocity: 0.5,
-      options: [],
-      animations: {
-        self.tableView.frame.origin.y = CGFloat(-300)
-        self.backgroundView.alpha = self.configuration.maskBackgroundOpacity },
-      completion: nil
-    )
-  }
-  
-  func hideMenu() {
-    // Rotate arrow
-    self.rotateArrow()
-    
-    self.isShown = false
-    
-    // Change background alpha
-    self.backgroundView.alpha = self.configuration.maskBackgroundOpacity
-    
-    UIView.animate(
-      withDuration: self.configuration.animationDuration * 1.5,
-      delay: 0,
-      usingSpringWithDamping: 0.7,
-      initialSpringVelocity: 0.5,
-      options: [],
-      animations: {
-        self.tableView.frame.origin.y = CGFloat(-200)
-    }, completion: nil
-    )
-    
-    // Animation
-    UIView.animate(
-      withDuration: self.configuration.animationDuration,
-      delay: 0,
-      options: UIViewAnimationOptions(),
-      animations: {
-        self.tableView.frame.origin.y = -CGFloat(self.items.count) * self.configuration.cellHeight - 300
-        self.backgroundView.alpha = 0 },
-      completion: { _ in
-        if self.isShown == false && self.tableView.frame.origin.y == -CGFloat(self.items.count) * self.configuration.cellHeight - 300 {
-          self.menuWrapper.isHidden = true
-        }
-    })
-  }
-  
-  func rotateArrow() {
-    UIView.animate(withDuration: self.configuration.animationDuration, animations: {[weak self] () -> () in
-      if let selfie = self {
-        selfie.menuArrow.transform = selfie.menuArrow.transform.rotated(by: 180 * CGFloat(Double.pi/180))
-      }
-    })
-  }
-  
-  func setMenuTitle(_ title: String) {
-    self.menuTitle.text = title
-  }
-  
-  func menuButtonTapped(_ sender: UIButton) {
-    self.isShown == true ? hideMenu() : showMenu()
   }
 }
